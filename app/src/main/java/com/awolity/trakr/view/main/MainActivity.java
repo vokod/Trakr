737843d1e083db6e746fdbe59ebdcfd6f5923de6
vokod <vokoda@gmail.com>
2018-08-07 15:06:31 +0200

    fixed issue, that right after enabling location permission, the locationn wasn't shown

--- app/src/main/java/com/awolity/trakr/view/main/MainActivity.java
+++ app/src/main/java/com/awolity/trakr/view/main/MainActivity.java
@@ -440,6 +440,14 @@ public class MainActivity extends AppCompatActivity
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // permission was granted, yay!
+                    if (googleMap != null) {
+                        try {
+                            googleMap.setMyLocationEnabled(true);
+                        } catch (SecurityException e) {
+                            Crashlytics.logException(e);
+                            // MyLog.e(TAG, e.getLocalizedMessage());
+                        }
+                    }
                 } else {
                     // permission denied, boo!
                 }
