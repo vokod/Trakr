8a68c50f7d7d46d776f1e7e9ea359b85685aae17
vokod <vokoda@gmail.com>
2018-08-16 14:13:59 +0200

    created settings with custom views

--- app/src/main/java/com/awolity/trakr/view/settings/SettingsActivity.java
+++ app/src/main/java/com/awolity/trakr/view/settings/SettingsActivity.java
@@ -6,8 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.awolity.trakr.R;
+import com.awolity.trakr.customviews.ButtonSetting;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private ButtonSetting loginBs, logoutBs, deleteBs;
 
     public static Intent getStarterIntent(Context context){
         Intent intent = new Intent(context, SettingsActivity.class);
@@ -18,5 +21,24 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setupWidgets();
+    }
+
+    private void setupWidgets(){
+        loginBs = findViewById(R.id.bs_login);
+        logoutBs = findViewById(R.id.bs_logout);
+        deleteBs = findViewById(R.id.bs_delete_account);
+
+        loginBs.setup(getString(R.string.setting_label_login),
+                getString(R.string.settings_description_login),
+                R.drawable.ic_login);
+
+        logoutBs.setup(getString(R.string.setting_label_logout),
+                getString(R.string.settings_description_logout),
+                R.drawable.ic_logout);
+
+        deleteBs.setup(getString(R.string.setting_label_delete_account),
+                getString(R.string.settings_description_delete_account),
+                R.drawable.ic_delete_account);
     }
 }
