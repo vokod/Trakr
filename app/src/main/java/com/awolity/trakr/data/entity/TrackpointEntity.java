14134ce1048f965f2120022be52a158c1ab25282
vokod <vokoda@gmail.com>
2018-08-31 11:16:15 +0200

    changed property names in firebase - this will decrease bandwidth usage

--- app/src/main/java/com/awolity/trakr/data/entity/TrackpointEntity.java
+++ app/src/main/java/com/awolity/trakr/data/entity/TrackpointEntity.java
@@ -8,6 +8,8 @@ import android.arch.persistence.room.PrimaryKey;
 import android.location.Location;
 import android.support.annotation.NonNull;
 
+import com.google.firebase.database.PropertyName;
+
 @SuppressWarnings("WeakerAccess")
 
 @Entity(tableName = "trackpoint_table",
@@ -23,115 +25,151 @@ public class TrackpointEntity {
     @NonNull
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "trackpoint_id")
+    @PropertyName("id")
     long trackpointId;
+    @PropertyName("tid")
     @ColumnInfo(name = "track_id")
     long trackId;
+    @PropertyName("t")
     long time;
+    @PropertyName("lat")
     double latitude;
+    @PropertyName("lon")
     double longitude;
+    @PropertyName("alt")
     double altitude;
+    @PropertyName("ult")
     @ColumnInfo(name = "altitude_unfiltered")
     double unfilteredAltitude;
+    @PropertyName("b")
     double bearing;
+    @PropertyName("s")
     double speed;
     @ColumnInfo(name = "speed_unfiltered")
+    @PropertyName("us")
     double unfilteredSpeed;
+    @PropertyName("ac")
     double accuracy;
+    @PropertyName("d")
     double distance;
 
     @NonNull
+    @PropertyName("id")
     public long getTrackpointId() {
         return trackpointId;
     }
 
+    @PropertyName("id")
     public void setTrackpointId(@NonNull long trackpointId) {
         this.trackpointId = trackpointId;
     }
 
+    @PropertyName("tid")
     public long getTrackId() {
         return trackId;
     }
 
+    @PropertyName("tid")
     public void setTrackId(long trackId) {
         this.trackId = trackId;
     }
 
+    @PropertyName("t")
     public long getTime() {
         return time;
     }
 
+    @PropertyName("t")
     public void setTime(long time) {
         this.time = time;
     }
 
+    @PropertyName("lat")
     public double getLatitude() {
         return latitude;
     }
 
+    @PropertyName("lat")
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+    @PropertyName("lon")
     public double getLongitude() {
         return longitude;
     }
 
+    @PropertyName("lon")
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
+    @PropertyName("alt")
     public double getAltitude() {
         return altitude;
     }
 
+    @PropertyName("alt")
     public void setAltitude(double altitude) {
         this.altitude = altitude;
     }
 
+    @PropertyName("ult")
     public double getUnfilteredAltitude() {
         return unfilteredAltitude;
     }
 
+    @PropertyName("ult")
     public void setUnfilteredAltitude(double unfilteredAltitude) {
         this.unfilteredAltitude = unfilteredAltitude;
     }
 
+    @PropertyName("b")
     public double getBearing() {
         return bearing;
     }
 
+    @PropertyName("b")
     public void setBearing(double bearing) {
         this.bearing = bearing;
     }
 
+    @PropertyName("s")
     public double getSpeed() {
         return speed;
     }
 
+    @PropertyName("s")
     public void setSpeed(double speed) {
         this.speed = speed;
     }
 
+    @PropertyName("us")
     public double getUnfilteredSpeed() {
         return unfilteredSpeed;
     }
 
+    @PropertyName("us")
     public void setUnfilteredSpeed(double unfilteredSpeed) {
         this.unfilteredSpeed = unfilteredSpeed;
     }
 
+    @PropertyName("ac")
     public double getAccuracy() {
         return accuracy;
     }
 
+    @PropertyName("ac")
     public void setAccuracy(double accuracy) {
         this.accuracy = accuracy;
     }
 
+    @PropertyName("d")
     public double getDistance() {
         return distance;
     }
 
+    @PropertyName("d")
     public void setDistance(double distance) {
         this.distance = distance;
     }
